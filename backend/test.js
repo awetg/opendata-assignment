39d7d5b9b619ff91f636c5ec6a0bb250ae7e5009
Awet Ghebrelassie <awetghebreslassie@gmail.com>
2020-05-06 21:11:32 +0300

    testing github action trigers

--- backend/test.js
+++ backend/test.js
@@ -13,3 +13,17 @@ describe("GET /", function () {
       });
   });
 });
+
+// testing the only valid api end point for getting sensor data, expecting empty object or array of sensor data
+// describe("GET /", function () {
+//   it("it shoud return empty json object or array of array of json data and status code 200", (done) => {
+//     supertest(app)
+//       .get("/api/events")
+//       .expect(200)
+//       .expect({})
+//       .end((err, res) => {
+//         if (err) done(err);
+//         done();
+//       });
+//   });
+// });
